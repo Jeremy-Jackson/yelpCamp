8eeaa5ded376fcfe2677524c16849bc3081052d3
Jeremy Jackson <wendigo9643@gmail.com>
2020-11-14 15:18:21 -0500

    third commit

---
+++ seeds/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const {places, descriptors} = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", () => {
+    console.log("Database connected m'lord.");
+});
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for(let i = 0; i < 50; i++){
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            title: `${sample(descriptors)} ${sample(places)}`,
+            location: `${cities[random1000].city}, ${cities[random1000].state}`
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+});
