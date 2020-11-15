389095423982b19d4b4c5d62b360e48b895a5e11
Jeremy Jackson <wendigo9643@gmail.com>
2020-11-15 13:12:38 -0500

    Fourth commit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
-const campground = require('./models/campground');
 
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -21,7 +21,7 @@ db.once("open", () => {
 
 const app = express();
 
-
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -65,7 +65,7 @@ app.put('/campgrounds/:id', async (req, res) => {
 
 app.delete('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
-    await campground.findByIdAndDelete(id);
+    await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 })
 
--- seeds/index.js
+++ seeds/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const cities = require('./cities');
-const {places, descriptors} = require('./seedHelpers');
+const {
+    places,
+    descriptors
+} = require('./seedHelpers');
 const Campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -21,11 +24,15 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i = 0; i < 50; i++){
+    for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
-            location: `${cities[random1000].city}, ${cities[random1000].state}`
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem, sed corrupti porro repudiandae quidem minus recusandae unde fugit, harum itaque, voluptates consequuntur optio.Alias unde repellat aliquid, excepturi tempora fugit.",
+            price
         })
         await camp.save();
     }
